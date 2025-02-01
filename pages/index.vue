30e747bdbc21e51af9a4ab578885b8c830aa9d7f
jcn <justcloudnative@gmail.com>
2025-02-01 12:20:12 +0100

    feat: test usefetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+
+// from typicode get posts
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const { data, error } = useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts')
+
+</script>
 
 <template>
   <div>
     <h1>This is a for learning nuxt </h1>
+    <ul>
+      <li v-for="post in data" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
   </div>
 </template>
